991a6f4f831ec7162fd14893a611f7358ebeb486
oussamaNAJIH <oussamanajih6@gmail.com>
2023-06-16 15:02:31 -0700

    aplph in lower and upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,6 +9,9 @@ int main(void)
 char Al[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int n = strlen(Al);
 for (int i = 0; i < n; i++)
+{
 putchar(Al[i]);
+}
+putchar('\n');
 return (0);
 }

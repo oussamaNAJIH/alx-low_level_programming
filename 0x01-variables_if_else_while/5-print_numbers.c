d87fc7f4c7f27fb04b82cc70008c6d1f5044e05e
oussamaNAJIH <oussamanajih6@gmail.com>
2023-06-16 16:01:29 -0700

    numbers 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Displays the alphabet without q and e.
+ * Return: return 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (int i = 0; i <= 9; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}

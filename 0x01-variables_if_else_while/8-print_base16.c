4c8cdb9ed6fe70900639afa3ba9fa90c4d9d3fc7
oussamaNAJIH <oussamanajih6@gmail.com>
2023-06-16 16:46:24 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Displays the alphabet without q and e.
+ * Return: return 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+for (i = 97; i < 103; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}

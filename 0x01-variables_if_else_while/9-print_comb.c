5f0c37d94be7545641e93c7e0c496edbeabc9d00
oussamaNAJIH <oussamanajih6@gmail.com>
2023-06-16 17:00:11 -0700

    persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Displays the alphabet without q and e.
+ * Return: return 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+if (i != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}

75db6b5daef6872487e40d0adecdd991cba21f7b
oussamaNAJIH <oussamanajih6@gmail.com>
2023-06-20 08:26:49 -0700

    functions printing alpha in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet -  displays alphabet in lower cases.
+ * Return: return 0 (Success)
+ */
+void print_alphabet(void)
+{
+int n;
+for (n = 97; n <= 122; n++)
+{
+putchar(n);
+}
+putchar('\n');
+}

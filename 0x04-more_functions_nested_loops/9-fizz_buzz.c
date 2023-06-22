ac2b21eec716646f859504cb1862694604a5173e
oussamaNAJIH <oussamanajih6@gmail.com>
2023-06-22 10:53:28 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,49 +1,34 @@
 # include <stdio.h>
-
 /**
  * main - prints the numbers from 1 to 100
  * for multiples of three print Fizz instead of the number
  * for the multiples of five print Buzz instead of the number
  * Return: returns 0 (Success)
  */
+
 int main(void)
 {
-int i, mod;
-for (i = 1; i <= 99; i++)
+int i;
+for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
+if (i % 3 == 0 && i % 5 != 0)
 {
-putchar(70);
-putchar(105);
-putchar(122);
-putchar(122);
+printf(" Fizz");
 }
-else if (i % 5 == 0)
+else if (i % 5 == 0 && i % 3 != 0)
 {
-putchar(66);
-putchar(117);
-putchar(122);
-putchar(122);
+printf(" Buzz");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf(" FizzBuzz");
 }
 else
 {
-if (i > 9)
-{
-mod = i % 10;
-putchar((i / 10) + '0');
-putchar(mod + '0');
-}
-else
-{
-putchar(i + '0');
+printf(" %d", i);
 }
 }
-putchar(32);
-}
-putchar(66);
-putchar(117);
-putchar(122);
-putchar(122);
-putchar('\n');
+printf("\n");
 return (0);
 }
+

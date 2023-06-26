f7af2632ad09bc1086acafee96ad061055fb6aa3
oussamaNAJIH <oussamanajih6@gmail.com>
2023-06-26 09:47:12 -0700

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: string print
+ */
+
+void _puts(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+}
+putchar('\n');
+}

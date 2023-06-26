a969aeac5e87df076e66f4147b0e084ab4a29f03
oussamaNAJIH <oussamanajih6@gmail.com>
2023-06-26 10:30:23 -0700

    inverse a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,8 +7,14 @@
 
 void print_rev(char *s)
 {
-int leng = _strlen(s);
-int i;
+int leng = 0;
+int i, j;
+j = 0;
+while (s[j] != '\0')
+{
+leng++;
+j++;
+}
 for (i = leng - 1; i >= 0; i--)
 {
 _putchar(s[i]);

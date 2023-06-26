e8bc8a30faefeab03b9fdea6d95d8aeff2a64543
oussamaNAJIH <oussamanajih6@gmail.com>
2023-06-26 11:42:51 -0700

    other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,12 +12,9 @@ while (str[leng] != '\0')
 {
 leng++;
 }
-for (i = 0; i <= leng - 1; i++)
-{
-if (str[i] % 2 == 0)
+for (i = 0; i <= leng - 1; i += 2)
 {
 _putchar(str[i]);
-}
 }
 _putchar('\n');
 }

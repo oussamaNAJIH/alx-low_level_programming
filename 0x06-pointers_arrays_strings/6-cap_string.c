44359e201722a33e3738ee9b25ac1960298766a8
oussamaNAJIH <oussamanajih6@gmail.com>
2023-06-29 11:11:42 -0700

    trying to cap a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
- * cap_string - Capitalizes all words of a string
- * @str: The string to be capitalized
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
  * Return: returns the string capitalized
  */
 char *cap_string(char *str)

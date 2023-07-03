fe7df4f19e341fe9fc50378c7c941a73193a2f56
oussamaNAJIH <oussamanajih6@gmail.com>
2023-07-03 10:33:19 -0700

    search char in str

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,3 @@
-#include <stddef.h>
 #include "main.h"
 /**
  * _strchr - locates a character in a string
@@ -10,13 +9,10 @@
 char *_strchr(char *s, char c)
 {
 int i = 0;
-while (*(s + i) != '\0')
+for (; s[i] >= '\0'; i++)
 {
-if (*(s + i) == c)
-{
-return (s + i);
+if (s[i] == c)
+return (&s[i]);
 }
-i++;
-}
-return (NULL);
+return (0);
 }

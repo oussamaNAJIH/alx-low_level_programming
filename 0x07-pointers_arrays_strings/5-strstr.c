95ef3ca7745d23c1846b848eb4eef03323bf0700
oussamaNAJIH <oussamanajih6@gmail.com>
2023-07-03 13:13:44 -0700

    search substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,30 +8,24 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-int i = 0;
-while (*(haystack + i) != '\0')
+if (*needle == '\0')
 {
-int j = 0;
-while (*(needle + j) != '\0')
-{
-if (*(haystack + i) == *(needle + j))
-{
-int x, y;
-for (x = i, y = j; *(haystack + x) != '\0' && *(needle + y) != '\0'; x++, y++)
-{
-if (*(haystack + x) != *(needle + y))
-{
-break;
+return (haystack);
 }
-}
-if (*(needle + y) == '\0')
+while (*haystack != '\0')
 {
-return (haystack + i);
+char *p1 = haystack;
+char *p2 = needle;
+while (*p2 != '\0' && *p1 == *p2)
+{
+p1++;
+p2++;
 }
+if (*p2 == '\0')
+{
+return (haystack);
 }
-j++;
-}
-i++;
+haystack++;
 }
 return (0);
 }

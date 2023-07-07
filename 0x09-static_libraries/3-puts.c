f4b1a8edcc9b687bc0c511061d087b930f7e3efe
oussamaNAJIH <oussamanajih6@gmail.com>
2023-07-07 12:54:26 -0700

    static library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -4,14 +4,11 @@
  * _puts - prints a string, followed by a new line
  * @s: string print
  */
-
 void _puts(char *s)
 {
-int i = 0;
-while (s[i] != '\0')
+while (*s != '\0')
 {
-_putchar(s[i]);
-i++;
+_putchar(*s++);
 }
 _putchar('\n');
 }

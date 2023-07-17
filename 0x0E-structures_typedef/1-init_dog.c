c1b9e3e3df88cd7ad4bc3a7329f0d0378ca1d0d8
oussamaNAJIH <oussamanajih6@gmail.com>
2023-07-17 06:20:45 -0700

    initializing first element

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog -  initializes a variable of type struct dog
+ * @d: variable of type struct dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}

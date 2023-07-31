68e417922417ad8e4b2e76813a5455dedaf7a142
oussamaNAJIH <oussamanajih6@gmail.com>
2023-07-31 11:24:04 -0700

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,10 @@
 void free_listint2(listint_t **head)
 {
 listint_t *current, *next;
-current = *head;
 if (head == NULL)
 return;
 
+current = *head;
 while (current != NULL)
 {
 next = current->next;

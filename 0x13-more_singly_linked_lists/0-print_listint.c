b5859d438acfa4dc81bacd7eaa4b5ae349918035
oussamaNAJIH <oussamanajih6@gmail.com>
2023-07-31 10:24:37 -0700

    print linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,6 +12,12 @@ size_t print_listint(const listint_t *h)
 int count = 0;
 const listint_t *tmp;
 tmp = h;
+if (tmp == NULL)
+{
+return (0);
+}
+else
+{
 while (tmp != NULL)
 {
 printf("%d\n", tmp->n);
@@ -19,4 +25,5 @@ count++;
 tmp = tmp->next;
 }
 return (count);
+}
 }

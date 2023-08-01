69bc8b0e5288bb560781d8703d7979c5ea390a8b
oussamaNAJIH <oussamanajih6@gmail.com>
2023-08-01 03:32:52 -0700

    print nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,19 +11,16 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
 listint_t *tmp = head;
 unsigned int count = 0;
 while (tmp != NULL)
 {
-tmp = tmp->next;
-count++;
 if (count == index)
 {
-new_node->n = tmp->n;
-new_node->next = tmp->next;
-return (new_node);
+return (tmp);
 }
+tmp = tmp->next;
+count++;
 }
 return (NULL);
 }

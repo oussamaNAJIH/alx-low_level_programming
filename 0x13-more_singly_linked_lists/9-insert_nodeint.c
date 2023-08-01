de89f85645838851f3f0a12d52af3b413fd4e856
oussamaNAJIH <oussamanajih6@gmail.com>
2023-08-01 04:32:38 -0700

    insert node at given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,9 +13,9 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 unsigned int i;
-listint_t *tmp = *head;
-listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
-new_node->next = NULL;
+listint_t *new_node;
+listint_t *temp = *head;
+new_node = malloc(sizeof(listint_t));
 if (new_node == NULL || head == NULL)
 {
 return (NULL);
@@ -28,15 +28,15 @@ new_node->next = *head;
 *head = new_node;
 return (new_node);
 }
-else
+for (i = 0; temp && i < idx; i++)
 {
-for (i = 0; i < idx - 1 && tmp != NULL; i++)
+if (i == idx - 1)
 {
-tmp = tmp->next;
-}
-new_node->next = tmp->next;
-tmp->next = new_node;
+new_node->next = temp->next;
+temp->next = new_node;
 return (new_node);
+}
+temp = temp->next;
 }
 return (NULL);
 }

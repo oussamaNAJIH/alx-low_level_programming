59042d28c29d9674f50e779eeea2de2bd2d62a7e
oussamaNAJIH <oussamanajih6@gmail.com>
2023-08-07 17:22:04 -0700

    copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -33,20 +33,15 @@ int fd_from, fd_to;
 ssize_t bytes_read, bytes_written;
 char buffer[BUFFER_SIZE];
 if (argc != 3)
-{
 exit_with_error("Usage: cp file_from file_to", "", 97);
-}
 
 fd_from = open(argv[1], O_RDONLY);
-if (fd_from == -1) {
+if (fd_from == -1)
 exit_with_error("Error: Can't read from file %s", argv[1], 98);
-}
 
 fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (fd_to == -1)
-{
 exit_with_error("Error: Can't write to file %s", argv[2], 99);
-}
 
 while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 {
@@ -58,18 +53,13 @@ exit_with_error("Error: Can't write to file %s", argv[2], 99);
 }
 
 if (bytes_read == -1)
-{
 exit_with_error("Error: Can't read from file %s", argv[1], 98);
-}
 
 if (close(fd_from) == -1)
-{
 exit_with_error("Error: Can't close fd %d", "", 100);
-}
 
-if (close(fd_to) == -1) {
+if (close(fd_to) == -1)
 exit_with_error("Error: Can't close fd %d", "", 100);
-}
 
 return (0);
 }

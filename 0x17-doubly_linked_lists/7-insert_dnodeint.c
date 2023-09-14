e8c07c7c7a55d11f1655d4d532f1a0aea83547f8
oussamaNAJIH <oussamanajih6@gmail.com>
2023-09-14 20:08:26 +0200

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -6,43 +6,55 @@
  * @index: the index of the node
  * Return: returns the data of the nth node
  */
-
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-dlistint_t *tmp = head;
-unsigned int i = 0;
-while (tmp != NULL)
-{
-if (i == index)
-{
-return (tmp);
+    dlistint_t *tmp = head;
+    unsigned int i = 0;
+    while (tmp != NULL)
+    {
+        if (i == index)
+        {
+            return (tmp);
+        }
+        tmp = tmp->next;
+        i++;
+    }
+    return (NULL);
 }
-tmp = tmp->next;
-i++;
-}
-return (NULL);
-}
-
 
 /**
- * insert_dnodeint_at_index -  inserts a new node at a given position
- * @h: a dlistint_t list
- * @idx: the data of the node to add
- * @n: data of the node to add
- * Return: returns the address of the new node
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: a pointer to a pointer to the head of a dlistint_t list
+ * @idx: the index at which to insert the new node
+ * @n: the data of the new node
+ * Return: returns the address of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *tmp = get_dnodeint_at_index(*h, idx);
-dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
-new_node->n = n;
-if (new_node == NULL || tmp == NULL)
+dlistint_t *new_node;
+dlistint_t *tmp = get_dnodeint_at_index(*h, idx - 1);
+if (idx == 0)
+{
+add_dnodeint(h, n);
+}
+new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+if (new_node == NULL)
 {
 return (NULL);
 }
-new_node->prev = tmp->prev;
-new_node->next = tmp;
-tmp->prev = new_node;
-return (*h);
+new_node->n = n;
+tmp = get_dnodeint_at_index(*h, idx - 1);
+if (tmp == NULL)
+{
+free(new_node);
+return (NULL);
+}
+new_node->prev = tmp;
+new_node->next = tmp->next;
+if (tmp->next != NULL)
+{
+tmp->next->prev = new_node;
+}
+tmp->next = new_node;
+return (new_node);
 }

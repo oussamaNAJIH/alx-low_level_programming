37c94b41855fe1b8644fbe95e4df86da13a5f07b
oussamaNAJIH <oussamanajih6@gmail.com>
2023-09-14 18:23:42 +0200

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,17 +6,20 @@
  * Return: returns the number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h){
-    int num;
-    const dlistint_t* tmp;
-    if (h == NULL){
-        return 0;
-    }
-    tmp = h;
-    while (tmp != NULL){
-        printf("%d", tmp->n);
-        tmp = tmp->next;
-        num++;
-    }
-    return (num);
+size_t print_dlistint(const dlistint_t *h)
+{
+int num = 0;
+const dlistint_t* tmp;
+if (h == NULL)
+{
+return (0);
+}
+tmp = h;
+while (tmp != NULL)
+{
+printf("%d\n", tmp->n);
+tmp = tmp->next;
+num++;
+}
+return (num);
 }

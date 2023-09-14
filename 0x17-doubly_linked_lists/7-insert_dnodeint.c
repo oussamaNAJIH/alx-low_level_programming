4ddfb0017f18a54023cb03dda2d8e0ed9e453afe
oussamaNAJIH <oussamanajih6@gmail.com>
2023-09-14 20:10:39 +0200

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,18 +8,18 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    dlistint_t *tmp = head;
-    unsigned int i = 0;
-    while (tmp != NULL)
-    {
-        if (i == index)
-        {
-            return (tmp);
-        }
-        tmp = tmp->next;
-        i++;
-    }
-    return (NULL);
+dlistint_t *tmp = head;
+unsigned int i = 0;
+while (tmp != NULL)
+{
+if (i == index)
+{
+return (tmp);
+}
+tmp = tmp->next;
+i++;
+}
+return (NULL);
 }
 
 /**

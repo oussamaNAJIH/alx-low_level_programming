87b6a6e5c1b8518f6a9f3af658e96d251b79dc84
oussamaNAJIH <oussamanajih6@gmail.com>
2023-10-06 20:15:36 +0200

    insert element to hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives you the index of a key
+ * @key: the key
+ * @size: the size of the array
+ * Return: returns the index
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+}

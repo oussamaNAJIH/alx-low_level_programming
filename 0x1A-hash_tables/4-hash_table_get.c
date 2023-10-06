d5cee152043e132d66092fbecb45bbf83b3eb354
oussamaNAJIH <oussamanajih6@gmail.com>
2023-10-06 21:37:48 +0200

    search a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -5,13 +5,13 @@
  * hash_table_get - retrieves a value associated with a key.
  * @ht:is the hash table you want to look into
  * @key: is the key you are looking for
- * Return: value associated with the element, or NULL if key couldn’t be found
+ * Return: value associated with the element
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 int index;
-hash_node_t* current_node;
+hash_node_t *current_node;
 if (!ht || !key)
 {
 return (NULL);

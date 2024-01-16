528a264bd6c7f895077d05f8a540634cba4edbec
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-16 16:32:48 +0100

    Linear search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,6 +14,8 @@ int binary_search(int *array, size_t size, int value)
 	size_t r = size - 1;
 	size_t i;
 
+	if (array == NULL || size == 0)
+		return (-1);
 	while (l <= r)
 	{
 		size_t mid = l + (r - l) / 2;

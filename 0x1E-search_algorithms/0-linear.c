d26d906a0bc131306281bfd3622756db56ba5241
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-16 16:24:13 +0100

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,15 +11,13 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
-
-	if (array == NULL)
-		return (-1);
-	for (i = 0; i < size; i++)
+	size_t i = 0;
+	while (i < size)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
+		i++;
 	}
 	return (-1);
 

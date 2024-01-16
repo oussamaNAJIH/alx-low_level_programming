70abacd3ab82ca9cf2b8bf12a5fbe4fa05c793f9
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-16 17:15:33 +0100

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,7 +12,7 @@
 int jump_search(int *array, size_t size, int value)
 {
 	size_t step = sqrt(size);
-	size_t start = 0, end = 0;
+	size_t start = 0, end = step;
 	size_t i;
 
 	while (end < size && array[end] < value)
